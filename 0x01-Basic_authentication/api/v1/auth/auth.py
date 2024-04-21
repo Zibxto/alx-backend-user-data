46a3aae4b8e0edf05bed68d814e655234e15b564
zibxto <christianaziba@gmail.com>
2024-04-21 22:52:23 +0100

    solve task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Auth module for the API
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Manage authenication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines whether a given path requires authentication or not
+        Args:
+            - path(str): Url path to be checked
+            - excluded_paths(List of str): List of paths that do not require
+              authentication
+        Return:
+            - True if path is not in excluded_paths, else False
+        """
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """Returns the authorization header from a request object"""
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns a User instance from information from a request object"""
+        return None
